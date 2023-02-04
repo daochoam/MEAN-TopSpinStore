f28829b731dc045f0dc3481ffa15b17556486d9a
daochoam <daochoam@unal.edu.co>
2023-02-03 23:23:37 -0500

    Update app.modules.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,11 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-import { PosterItemComponent } from './components/poster-item/poster-item.component';
-import { PageBallsComponent } from './components/page-balls/page-balls.component';
+
 import { BallsComponent } from './Page/balls/balls.component';
 import { RubbersComponent } from './Page/rubbers/rubbers.component';
 import { BladesComponent } from './Page/blades/blades.component';
@@ -17,23 +13,26 @@ import { AdminComponent } from './Page/admin/admin.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { HeaderComponent } from './Components/header/header.component';
 import { PosterItemsComponent } from './Components/poster-items/poster-items.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegisterComponent } from './Components/register/register.component';
+import { HomeComponent } from './Page/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    // Project Pages
     HomeComponent,
-    RegisterComponent,
-    LoginComponent,
-    PosterItemComponent,
-    PageBallsComponent,
     BallsComponent,
     RubbersComponent,
     BladesComponent,
     TablesComponent,
     UserComponent,
     AdminComponent,
+    // Project Componentes
     FooterComponent,
     HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
     PosterItemsComponent
   ],
   imports: [
