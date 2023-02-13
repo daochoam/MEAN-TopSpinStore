cc90af7c312c0c434d23175d89207f1f4df1e0bb
Goliat86 <117695827+Goliat86@users.noreply.github.com>
2023-02-12 21:01:02 -0500

    Delete backend/api/Routes directory

--- backend/api/Routes/ItemsRoutes.js
+++
@@ -1,6 +0,0 @@
-var ItemsControllers = require(__dirname+'/../Controller/ItemsControllers.js').items
-var express = require('express')
-const routes = express.Router()
-
-
-module.exports = routes
--- backend/api/Routes/UsersRoutes.js
+++
@@ -1,9 +0,0 @@
-var UsersControllers = require(__dirname+'/../Controller/UsersControllers.js').users
-var express = require('express')
-const routes = express.Router()
-
-routes.post("/register", function(req, res){
-    UsersControllers.RegisterUsers(req, res)
-})
-
-module.exports = routes
