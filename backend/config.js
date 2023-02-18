1962dd3180f8b26c11656c6d78466b74d4f357fe
Yefferson Bermudez <tomasjefferson26@gmail.com>
2023-02-18 18:25:30 -0500

    update yefferson

--- backend/config.js
+++ backend/config.js
@@ -1,14 +1,10 @@
 var config = {}
 
 config.listablanca = [
-<<<<<<< HEAD
     'http://127.0.0.1:5000',
     'https://kit.fontawesome.com/',
-    'https://localhost:4200',
+    'http://localhost:4200',
 
-=======
-    'http://127.0.0.1:5000'
->>>>>>> a42921710bda8b6f016babe643e85e866038f8ae
 ]
 config.puerto = 3000
 config.db = "topspinstore"
